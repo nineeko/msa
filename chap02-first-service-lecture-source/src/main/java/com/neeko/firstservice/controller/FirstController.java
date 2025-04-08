97c414687fe76693d3c0f59c9ec9a8a7f655d87b
nineeko <gkxm1013@naver.com>
2025-04-08 11:13:16 +0900

    25.04.08 am 11:13

--- chap02-first-service-lecture-source/src/main/java/com/neeko/firstservice/controller/FirstController.java
+++ chap02-first-service-lecture-source/src/main/java/com/neeko/firstservice/controller/FirstController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/first-service")
 public class FirstController {
 
     private Environment environment;
